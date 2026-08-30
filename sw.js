576673444a94cdac69ca6f4d167ca26e19ed8c47
doctorjiemar0-dotcom <doctorjiemar0@gmail.com>
2026-08-30 20:33:38 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,19 @@
+const CACHE_NAME = 'whisk-portal-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
 
-self.addEventListener('fetch', (event) => {
-  // Basic offline caching pass-through
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
