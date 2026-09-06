b9492084f4d8c57441556a54d446c8e13dcf799c
doctorjiemar0-dotcom <doctorjiemar0@gmail.com>
2026-09-06 08:48:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,40 @@
-const CACHE_NAME = 'whisk-portal-v1';
+const CACHE_NAME = 'whisk-portal-v2';
+
 const urlsToCache = [
   './index.html',
   './manifest.json'
 ];
 
+// INSTALL
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
+// ACTIVATE
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames
+            .filter(cacheName => cacheName !== CACHE_NAME)
+            .map(cacheName => caches.delete(cacheName))
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+// FETCH
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(cachedResponse => {
+        return cachedResponse || fetch(event.request);
+      })
   );
 });
