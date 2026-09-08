7273957e0d197f3b9cd54c12c5220832565d48a6
doctorjiemar0-dotcom <doctorjiemar0@gmail.com>
2026-09-09 06:05:04 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,70 @@
-const CACHE_NAME = 'whisk-portal-v1';
-const urlsToCache = [
-  './index.html',
-  './manifest.json'
+const CACHE_NAME = "doctors-whisk-broom-client-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.jpg",
+  "./sw.js"
 ];
-self.addEventListener('install', event => {
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(APP_FILES);
+      })
+      .then(() => self.skipWaiting())
   );
 });
-self.addEventListener('fetch', event => {
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((cachedResponse) => {
+
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+
+          if (
+            !networkResponse ||
+            networkResponse.status !== 200 ||
+            networkResponse.type !== "basic"
+          ) {
+            return networkResponse;
+          }
+
+          const responseClone = networkResponse.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+
+          return networkResponse;
+        })
+        .catch(() => {
+          return caches.match("./index.html");
+        });
+    })
   );
 });
