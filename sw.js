869c670d0cac87c8c88f7c47b5111fb80ec58999
doctorjiemar0-dotcom <doctorjiemar0@gmail.com>
2026-09-12 21:22:08 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,70 +1,41 @@
-const CACHE_NAME = "doctors-whisk-broom-client-v1";
-
-const APP_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.jpg",
-  "./sw.js"
+const CACHE_NAME = 'Whisk-portal-v2';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(APP_FILES);
-      })
+      .then(cache => cache.addAll(urlsToCache))
       .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName))
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
       );
     }).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") {
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // Huwag pakialaman ang mga external request (tulad ng Firebase API, Firestore, Auth)
+  if (url.origin !== location.origin) {
     return;
   }
 
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request)
-        .then((networkResponse) => {
-
-          if (
-            !networkResponse ||
-            networkResponse.status !== 200 ||
-            networkResponse.type !== "basic"
-          ) {
-            return networkResponse;
-          }
-
-          const responseClone = networkResponse.clone();
-
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, responseClone);
-          });
-
-          return networkResponse;
-        })
-        .catch(() => {
-          return caches.match("./index.html");
-        });
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
